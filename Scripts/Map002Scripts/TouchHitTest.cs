0bc15d49879b977e95365f553e013145e78aa1f5
flashfangFyw <wjsfangyw0502@hotmail.com>
2018-02-07 14:24:42 +0800

    a8

--- Scripts/Map002Scripts/TouchHitTest.cs
+++ Scripts/Map002Scripts/TouchHitTest.cs
@@ -54,7 +54,7 @@ public class TouchHitTest : MonoBehaviour
         PutHitTest();
         //ControlCheck();
         TouchControl();
-        KeyControl();
+        //KeyControl();
     }
     void OnDisable()
     {
